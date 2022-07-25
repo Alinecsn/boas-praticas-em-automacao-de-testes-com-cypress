9e5dc09e976bec89b4459d7f55eb09fe33f4fc4f
Alinecsn <83730853+Alinecsn@users.noreply.github.com>
2022-07-25 08:55:00 -0300

    teste

--- cypress/integration/browser-testing/sample2.spec.js
+++ cypress/integration/browser-testing/sample2.spec.js
@@ -1,12 +1,24 @@
 describe('Browser testing bad practice', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      {fixture: 'stories'}
+    ).as('getStories')
+    
     cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
   })
 
-  it('tests a browser feature instead of an app feature', () => {
+  it('tests a browser feature instead of an app feature', () => { 
     cy.get('.table-row a')
       .first()
-      .click()
+      .should('have.attr', 'href','https://example.com/lc-jg')
+      .should('have.attr','target','_blank')
+
+      //cy.get('.table-row a')
+      //.first()
+      //.click()
 
     // Assert that a new tab was opened in the correct URL
   })
