859f342c1450a864969e6c806c7f5baba4ae9ad3
Alinecsn <83730853+Alinecsn@users.noreply.github.com>
2022-07-26 09:51:41 -0300

    atualização

--- cypress/integration/hardcoded-assertion/sample.spec.js
+++ cypress/integration/hardcoded-assertion/sample.spec.js
@@ -11,17 +11,27 @@ describe('Hardcoded assertion bad practice', () => {
   })
 
   it('searches', () => {
+    const {hits} = require('../../fixtures/stories')
     cy.search('cypress.io')
     cy.wait('@getStories')
 
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
+      .should('have.length', hits.length)
+      hits.forEach((hit,index) => {
+        cy.get('.table-row')
+        .eq(index)
+        .should('contain',hit.title)
+
+    /*/cy.fixture('stories').then(stories => {
+      cy.get('.table-row')
+      .as('tableRows')
+      .should('have.length', stories.hits.length)
+      stories.hits.forEach((hit,index) => {
+        cy.get('.table-row')
+        .eq(index)
+        .should('contain',hit.title)
+      });*/
+    })
   })
 })
